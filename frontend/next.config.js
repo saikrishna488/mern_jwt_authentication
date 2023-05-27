6b9901f5e499de81325ed8594fbe0e8509602e8b
Saikrishna Reddy <117559736+saikrishna488@users.noreply.github.com>
2023-05-28 00:55:42 +0530

    eight commit --testing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+          net: false,
+          tls: false,
+        };
+      }
+      return config;
+    },
+    poweredByHeader: false, // Optional: If you want to disable the "X-Powered-By" header.
+  };
+  
